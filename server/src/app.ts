02747f36c8fa3882ff947ebfdd194caa1cde2f0a
Alessio Mason <alessio.mason@me.com>
2024-01-29 14:59:49 +0100

    Fix to server serving client

--- server/src/app.ts
+++ server/src/app.ts
@@ -69,12 +69,13 @@ useSystemAPIs(app, isLoggedIn);
 useUsersAPIs(app, isLoggedIn);
 useAuthenticationAPIs(app, store, isLoggedIn);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+//if (process.env.NODE_ENV === "production") {
     const path = require("path");
+    app.use(express.static(path.resolve(__dirname, "../../client", "build")));
     app.get("*", (_req: Request, res: Response) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+        console.log(__dirname)
+        res.sendFile(path.resolve(__dirname, "../../client", "build", "index.html"));
     });
-}
+//}
 
 export default app;
