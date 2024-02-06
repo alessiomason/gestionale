d3988edf12437b34d02fc46163111c489cc638ac
Alessio Mason <alessio.mason@me.com>
2024-02-06 11:11:48 +0100

    Navbar style

--- client/src/navbar/MyNavbar.tsx
+++ client/src/navbar/MyNavbar.tsx
@@ -1,6 +1,7 @@
 import {useNavigate} from 'react-router-dom';
-import {Container, Navbar} from 'react-bootstrap';
+import {Button, Col, Container, Navbar, Row} from 'react-bootstrap';
 import './MyNavbar.css';
+import horizontalWhiteLogo from '../images/logos/horizontal_white_logo.png';
 
 function MyNavbar() {
     const navigate = useNavigate();
@@ -8,9 +9,17 @@ function MyNavbar() {
     return (
         <Navbar className="navbar fixed-top navbar-padding">
             <Container fluid>
-                <Navbar.Brand className='purple text' onClick={() => navigate("/")}>
-                    Technomake
-                </Navbar.Brand>
+                <Row className="navbar-row">
+                    <Col>
+                        <Navbar.Brand className='text' onClick={() => navigate("/")}>
+                            <img src={horizontalWhiteLogo} className="brand-image" alt="Logo di Technomake"/>
+                        </Navbar.Brand>
+                    </Col>
+
+                    <Col className="d-flex justify-content-end align-items-center">
+                        <Button className="light-glossy-button">Scheda personale</Button>
+                    </Col>
+                </Row>
             </Container>
         </Navbar>
     );
