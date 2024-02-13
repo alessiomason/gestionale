c7f6c61f5852f4dfa70a083918fb2d473dd2e5bc
Alessio Mason <alessio.mason@me.com>
2024-02-13 16:06:54 +0100

    Refactoring and fixed tests

--- client/src/users-management/UsersListPage.tsx
+++ client/src/users-management/UsersListPage.tsx
@@ -4,14 +4,11 @@ import userApis from "../api/userApis";
 import {Col, FloatingLabel, Form, InputGroup, Row, Table} from "react-bootstrap";
 import {
     CarFront,
-    Check2,
     CheckCircle,
     Clock,
     Coin,
     EnvelopeAt,
     Icon,
-    Key,
-    Link45deg,
     Person,
     PersonAdd,
     PersonBadge,
@@ -19,15 +16,12 @@ import {
     Telephone,
     XCircle
 } from "react-bootstrap-icons";
-import Copy from "../new-bootstrap-icons/Copy";
 import SwitchToggle from "./SwitchToggle";
 import "./UsersListPage.css";
 import NewUserPane from "./NewUserPane";
 import GlossyButton from "../buttons/GlossyButton";
 import Floppy from "../new-bootstrap-icons/Floppy";
-import dayjs from "dayjs";
-import {publicUrl} from "../api/apisValues";
-import LightGlossyButton from "../buttons/LightGlossyButton";
+import {RegisteredSection, NoRegistrationSection} from "./UsersListSections";
 
 function compareUsers(a: User, b: User) {
     // sort active first
@@ -277,67 +271,6 @@ function UsersListPage() {
                         </Form>
                     }
                 </Col>
-            </Row>
-        </>
-    );
-}
-
-function RegisteredSection(props: { user: User }) {
-    return (
-        <>
-            <Row className="mt-5">
-                <h3>Utente registrato</h3>
-            </Row>
-
-            <Row>
-                <Col>
-                    L'utente si è registrato {dayjs(props.user.registrationDate).format("dddd LL [alle] LT")}.
-                </Col>
-            </Row>
-            <Row className="mt-3">
-                <Col/>
-                <Col sm={8} className="d-flex justify-content-center">
-                    <GlossyButton icon={Key} onClick={() => {
-                    }}>Resetta password (non disponibile)</GlossyButton>
-                </Col>
-                <Col/>
-            </Row>
-        </>
-    );
-}
-
-function NoRegistrationSection(props: { user: User }) {
-    const registrationLink = `${publicUrl}signup/${props.user.registrationToken}`;
-    const [copied, setCopied] = useState(false);
-
-    async function handleCopy() {
-        await navigator.clipboard.writeText(registrationLink);
-        setCopied(true);
-    }
-
-    return (
-        <>
-            <Row className="mt-4">
-                <h3>Registrazione dell'utente</h3>
-                <p>Invia questo link all'utente per completare la procedura di registrazione.</p>
-                <p>Il link scade 7 giorni dopo la generazione:
-                    questo link scadrà {dayjs(props.user.tokenExpiryDate).format("dddd LL [alle] LT")}.</p>
-            </Row>
-
-            <Row>
-                <InputGroup className="mt-2">
-                    <InputGroup.Text><Link45deg/></InputGroup.Text>
-                    <FloatingLabel controlId="floatingInput" label="Link di registrazione">
-                        <Form.Control type="text"
-                                      placeholder="Link di registrazione"
-                                      value={registrationLink}
-                                      disabled/>
-                    </FloatingLabel>
-                    <LightGlossyButton icon={copied ? Check2 : (Copy as Icon)} className="input-group-button"
-                                       onClick={handleCopy}>
-                        {copied ? "Link copiato" : "Copia link"}
-                    </LightGlossyButton>
-                </InputGroup>
             </Row>
         </>
     );
