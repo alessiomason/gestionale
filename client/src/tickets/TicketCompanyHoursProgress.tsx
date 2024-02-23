5d07012f0692f85fc16fa651666443c0b2de1787
Alessio Mason <alessio.mason@me.com>
2024-02-23 11:23:19 +0100

    Ticket company colored progress bar

--- client/src/tickets/TicketCompanyHoursProgress.tsx
+++ client/src/tickets/TicketCompanyHoursProgress.tsx
@@ -1,15 +1,23 @@
 import {ProgressBar} from "react-bootstrap";
 import {TicketCompany} from "../models/ticketCompany";
-import {humanize} from "../functions";
+import "./TicketCompanyHoursProgress.css";
 
 interface TicketCompanyHoursProgressProps {
     ticketCompany: TicketCompany
 }
 
 function TicketCompanyHoursProgress(props: TicketCompanyHoursProgressProps) {
+    let className = "progress-bar-ok"
+    if (props.ticketCompany.remainingHoursPercentage <= 10) {
+        className = "progress-bar-red"
+    } else if (props.ticketCompany.remainingHoursPercentage <= 15) {
+        className = "progress-bar-warning"
+    }
+
     return (
         <ProgressBar now={props.ticketCompany.remainingHoursPercentage}
-                     label={`${Math.round(props.ticketCompany.remainingHoursPercentage)}%`}/>
+                     label={`${Math.round(props.ticketCompany.remainingHoursPercentage)}%`}
+                     className={className}/>
     );
 }
 
