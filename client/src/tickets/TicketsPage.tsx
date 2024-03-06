8bebd75940786b7326bcf37e7282e22bd2ba3fbe
Alessio Mason <alessio.mason@me.com>
2024-03-06 16:59:26 +0100

    Update ticket companies list upon company creation

--- client/src/tickets/TicketsPage.tsx
+++ client/src/tickets/TicketsPage.tsx
@@ -44,7 +44,12 @@ function TicketsPage() {
             setTicketCompanies(ticketCompanies => {
                 const newTicketCompanies = ticketCompanies;
                 const index = newTicketCompanies.findIndex(t => t.id === updatedTicketCompany.id);
-                newTicketCompanies[index] = updatedTicketCompany;
+
+                if (index === -1) {
+                    newTicketCompanies.push(updatedTicketCompany);
+                } else {
+                    newTicketCompanies[index] = updatedTicketCompany;
+                }
                 return newTicketCompanies;
             })
         } else {    // deleted company, refresh
