819994f5ec71adf576e6dd3ec2b98e76d824f979
Alessio Mason <alessio.mason@me.com>
2024-04-17 15:13:17 +0200

    Fix comment in backup.php

--- server/src/database/backup.php
+++ server/src/database/backup.php
@@ -1,5 +1,5 @@
 <?php
-// This file is not part of the Express server, but is meant to be uploaded on a Apache server where the backup are stored
+// This file is not part of the Express server, but is meant to be uploaded on an Apache server where the backups are stored
 
 if (!isset($_FILES["backupFile"])) {
     http_response_code(422);
