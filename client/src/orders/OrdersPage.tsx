fa986a2d302143ccb5f7f084dab95c52f38bece1
Alessio Mason <alessio.mason@me.com>
2024-05-14 12:38:52 +0200

    Orders mobile page

--- client/src/orders/OrdersPage.tsx
+++ client/src/orders/OrdersPage.tsx
@@ -1,17 +1,9 @@
 import React, {useEffect, useState} from "react";
-import {Col, Row, Table} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import {Col, Row} from "react-bootstrap";
 import GlossyButton from "../buttons/GlossyButton";
-import TextButton from "../buttons/TextButton";
 import {
-    ArrowLeftSquare,
-    ArrowRightSquare,
-    CaretDownFill,
-    CaretRight,
-    CaretUpFill,
     ClipboardPlus,
     ClipboardX,
-    FileEarmark,
     Funnel
 } from "react-bootstrap-icons";
 import Loading from "../Loading";
@@ -21,21 +13,22 @@ import OrdersFiltersModal from "./OrdersFiltersModal";
 import {Order} from "../models/order";
 import orderApis from "../api/orderApis";
 import {User} from "../models/user";
-import {formatDate} from "../functions";
 import dayjs from "dayjs";
 import "./OrdersPage.css";
+import {useMediaQuery} from "react-responsive";
+import OrdersTable from "./OrdersTable";
+
+export type PossibleSortingOptions = "name" | "job" | "supplier" | "deliveryDate";
 
 interface OrdersPageProps {
     readonly user: User
 }
 
-type PossibleSortingOptions = "name" | "job" | "supplier" | "deliveryDate";
 
 function OrdersPage(props: OrdersPageProps) {
+    const isMobile = useMediaQuery({maxWidth: 767});
+
     const [orders, setOrders] = useState<Order[]>([]);
-    const [pageNumber, setPageNumber] = useState(0);
-    const increasablePageNumber = (pageNumber + 1) * 100 <= orders.length;
-    const decreasablePageNumber = pageNumber > 0;
     const [nextOrderId, setNextOrderId] = useState(1);
     const [dirty, setDirty] = useState(true);
     const [loading, setLoading] = useState(true);
@@ -44,7 +37,7 @@ function OrdersPage(props: OrdersPageProps) {
         const orderId = sessionStorage.getItem("selectedOrderId");
         return orderId ? orders.find(o => o.id === parseInt(orderId)) : undefined;
     });
-    const shrunkTable = showingNewOrderPane || selectedOrder !== undefined;
+    const shrunkTable = isMobile || showingNewOrderPane || selectedOrder !== undefined;
 
     const [showFilterModal, setShowFilterModal] = useState(false);
     const [filteringOrderName, setFilteringOrderName] = useState<string | undefined>(() => {
@@ -68,8 +61,6 @@ function OrdersPage(props: OrdersPageProps) {
         const compOrder = sessionStorage.getItem("comparisonOrder");
         return compOrder as "asc" | "desc" | null ?? "desc";
     });
-
-    const navigate = useNavigate();
 
     // save all filters for the whole session
     useEffect(() => {
@@ -194,81 +185,6 @@ function OrdersPage(props: OrdersPageProps) {
         setShowingNewOrderPane(false);
     }
 
-    function increasePageNumber() {
-        if (increasablePageNumber) {
-            setPageNumber(prevPageNumber => prevPageNumber + 1);
-        }
-    }
-
-    function decreasePageNumber() {
-        if (decreasablePageNumber) {
-            setPageNumber(prevPageNumber => prevPageNumber - 1);
-        }
-    }
-
-    function filterOrders(value: Order) {
-        return (filteringOrderName === undefined || value.name.startsWith(filteringOrderName)) &&
-            (filteringJobId === undefined || value.job.id.startsWith(filteringJobId)) &&
-            (filteringSupplier === undefined || value.supplier.toLowerCase().includes(filteringSupplier.toLowerCase()));
-    }
-
-    function showCaret(header: PossibleSortingOptions) {
-        if (comparison === header) {
-            if (comparisonOrder === "asc") {
-                return <CaretUpFill/>;
-            }
-            return <CaretDownFill/>;
-        }
-        return <CaretRight/>;
-    }
-
-    function selectComparison(choice: PossibleSortingOptions) {
-        if (comparison === choice) {
-            setComparisonOrder(prevOrder => prevOrder === "asc" ? "desc" : "asc");
-        } else {
-            setComparison(choice);
-            setComparisonOrder(choice === "supplier" ? "asc" : "desc");
-        }
-    }
-
-    function compareOrdersByName(a: Order, b: Order) {
-        const yearComparison = a.year - b.year;
-        return yearComparison === 0 ? a.id - b.id : yearComparison;
-    }
-
-    function compareOrdersByDeliveryDate(a: Order, b: Order) {
-        if (a.scheduledDeliveryDate && b.scheduledDeliveryDate) {
-            return a.scheduledDeliveryDate.localeCompare(b.scheduledDeliveryDate);
-        } else if (a.scheduledDeliveryDate) {
-            return -1;
-        } else if (b.scheduledDeliveryDate) {
-            return 1;
-        }
-
-        return 0;
-    }
-
-    function compareOrders(a: Order, b: Order) {
-        switch (comparison) {
-            case "name": {
-                const nameComparison = compareOrdersByName(a, b);
-                return comparisonOrder === "asc" ? nameComparison : -1 * nameComparison;
-            }
-            case "job": {
-                const jobComparison = a.job.id.localeCompare(b.job.id);
-                return comparisonOrder === "asc" ? jobComparison : -1 * jobComparison;
-            }
-            case "supplier": {
-                const supplierComparison = a.supplier.localeCompare(b.supplier);
-                return comparisonOrder === "asc" ? supplierComparison : -1 * supplierComparison;
-            }
-            case "deliveryDate": {
-                const deliveryDateComparison = compareOrdersByDeliveryDate(a, b);
-                return comparisonOrder === "asc" ? deliveryDateComparison : -1 * deliveryDateComparison;
-            }
-        }
-    }
-
     return (
         <>
             <Row>
@@ -282,12 +198,12 @@ function OrdersPage(props: OrdersPageProps) {
                 </Col>
                 <Col/>
                 <Col sm={4}>
-                    {shrunkTable ?
+                    {showingNewOrderPane || selectedOrder ?
                         <GlossyButton icon={ClipboardX} onClick={handleCloseButton} className="new-user-button">
                             Chiudi</GlossyButton> :
                         <GlossyButton icon={Funnel} onClick={() => setShowFilterModal(true)}
-                                   className="new-user-button">
-                        Filtri</GlossyButton>}
+                                      className="new-user-button">
+                            Filtri</GlossyButton>}
                 </Col>
                 <OrdersFiltersModal show={showFilterModal} setShow={setShowFilterModal}
                                     filteringOrderName={filteringOrderName}
@@ -300,85 +216,14 @@ function OrdersPage(props: OrdersPageProps) {
                 <Col sm={shrunkTable ? 4 : 0}
                      className={shrunkTable ? "orders-page-first-column" : "me-4 orders-page-first-column"}>
 
-                    {loading ?
-                        <Loading/> :
-                        <Row className="glossy-background w-100">
-                            <Row className="mb-2">
-                                <Col className="d-flex justify-content-between">
-                                    <ArrowLeftSquare
-                                        className={decreasablePageNumber ? "clickable-arrow" : "unclickable-arrow"}
-                                        onClick={decreasePageNumber}/>
-                                    <p className="text-center">
-                                        Pagina {pageNumber + 1} di {Math.ceil(orders.length / 100)}
-                                    </p>
-                                    <ArrowRightSquare
-                                        className={increasablePageNumber ? "clickable-arrow" : "unclickable-arrow"}
-                                        onClick={increasePageNumber}/>
-                                </Col>
-                            </Row>
-
-                            <Table hover responsive className="orders-table">
-                                <thead>
-                                <tr>
-                                    <th className="comparable"
-                                        onClick={() => selectComparison("name")}>Ordine {showCaret("name")}</th>
-                                    <th className="comparable"
-                                        onClick={() => selectComparison("job")}>Commessa {showCaret("job")}</th>
-                                    {!shrunkTable && <th>Data</th>}
-                                    <th className="comparable"
-                                        onClick={() => selectComparison("supplier")}>Fornitore {showCaret("supplier")}</th>
-                                    {!shrunkTable && <>
-                                        <th>Descrizione</th>
-                                        <th>Presa in carico da</th>
-                                        <th className="comparable"
-                                            onClick={() => selectComparison("deliveryDate")}>Consegna {showCaret("deliveryDate")}</th>
-                                        <th>Evasa da</th>
-                                        <th>Evasa il</th>
-                                        <th>Allegato</th>
-                                    </>}
-                                </tr>
-                                </thead>
-
-                                <tbody>
-                                {orders
-                                    .filter(filterOrders)
-                                    .sort(compareOrders)
-                                    .slice(pageNumber * 100, (pageNumber + 1) * 100)
-                                    .map(order => {
-                                        let className = "";
-                                        if (order.id === selectedOrder?.id) {
-                                            className = "table-selected-row";
-                                        }
-                                        if (order.clearedBy && order.clearingDate) {
-                                            className += " cleared";
-                                        }
-
-                                        return (
-                                            <tr key={order.name} onClick={() => selectOrder(order)}
-                                                className={className}>
-                                                <td>{order.name}</td>
-                                                <td>{order.job.id}</td>
-                                                {!shrunkTable && <td>{formatDate(order.date)}</td>}
-                                                <td>{order.supplier}</td>
-                                                {!shrunkTable && <>
-                                                    <td>{order.description}</td>
-                                                    <td>{order.by.surname} {order.by.name}</td>
-                                                    <td>{formatDate(order.scheduledDeliveryDate)}</td>
-                                                    <td>{order.clearedBy?.surname} {order.clearedBy?.name}</td>
-                                                    <td>{formatDate(order.clearingDate)}</td>
-                                                    {order.uploadedFile ? <td>
-                                                        <TextButton icon={FileEarmark} onClick={() => {
-                                                            const attachmentOrderName = `${order.id}-${order.year.toString().substring(2)}`;
-                                                            navigate(`/order/${attachmentOrderName}/pdf`);
-                                                        }}>Apri</TextButton>
-                                                    </td> : <td/>}
-                                                </>}
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
-                            </Table>
-                        </Row>}
+                    {loading &&
+                        <Loading/>}
+                    {!loading && (!isMobile || (!selectedOrder && !showingNewOrderPane)) &&
+                        <OrdersTable orders={orders} shrunkTable={shrunkTable} selectedOrder={selectedOrder}
+                                     selectOrder={selectOrder} filteringOrderName={filteringOrderName}
+                                     filteringJobId={filteringJobId} filteringSupplier={filteringSupplier}
+                                     comparison={comparison} setComparison={setComparison}
+                                     comparisonOrder={comparisonOrder} setComparisonOrder={setComparisonOrder}/>}
                 </Col>
 
                 {showingNewOrderPane &&
---
+++ client/src/orders/OrdersTable.tsx
@@ -0,0 +1,194 @@
+import React, {useState} from "react";
+import {Col, Row, Table} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import {
+    ArrowLeftSquare,
+    ArrowRightSquare,
+    CaretDownFill,
+    CaretRight,
+    CaretUpFill,
+    FileEarmark
+} from "react-bootstrap-icons";
+import {PossibleSortingOptions} from "./OrdersPage";
+import TextButton from "../buttons/TextButton";
+import {formatDate} from "../functions";
+import {Order} from "../models/order";
+
+interface OrdersTableProps {
+    readonly orders: Order[]
+    readonly shrunkTable: boolean
+    readonly selectedOrder: Order | undefined
+    readonly selectOrder: (order: Order) => void
+    readonly filteringOrderName: string | undefined
+    readonly filteringJobId: string | undefined
+    readonly filteringSupplier: string | undefined
+    readonly comparison: PossibleSortingOptions
+    readonly setComparison: React.Dispatch<React.SetStateAction<PossibleSortingOptions>>
+    readonly comparisonOrder: "asc" | "desc"
+    readonly setComparisonOrder: React.Dispatch<React.SetStateAction<"asc" | "desc">>
+}
+
+function OrdersTable(props: OrdersTableProps) {
+    const [pageNumber, setPageNumber] = useState(0);
+    const increasablePageNumber = (pageNumber + 1) * 100 <= props.orders.length;
+    const decreasablePageNumber = pageNumber > 0;
+
+    const navigate = useNavigate();
+
+    function increasePageNumber() {
+        if (increasablePageNumber) {
+            setPageNumber(prevPageNumber => prevPageNumber + 1);
+        }
+    }
+
+    function decreasePageNumber() {
+        if (decreasablePageNumber) {
+            setPageNumber(prevPageNumber => prevPageNumber - 1);
+        }
+    }
+
+    function filterOrders(value: Order) {
+        return (props.filteringOrderName === undefined || value.name.startsWith(props.filteringOrderName)) &&
+            (props.filteringJobId === undefined || value.job.id.startsWith(props.filteringJobId)) &&
+            (props.filteringSupplier === undefined || value.supplier.toLowerCase().includes(props.filteringSupplier.toLowerCase()));
+    }
+
+    function showCaret(header: PossibleSortingOptions) {
+        if (props.comparison === header) {
+            if (props.comparisonOrder === "asc") {
+                return <CaretUpFill/>;
+            }
+            return <CaretDownFill/>;
+        }
+        return <CaretRight/>;
+    }
+
+    function selectComparison(choice: PossibleSortingOptions) {
+        if (props.comparison === choice) {
+            props.setComparisonOrder(prevOrder => prevOrder === "asc" ? "desc" : "asc");
+        } else {
+            props.setComparison(choice);
+            props.setComparisonOrder(choice === "supplier" ? "asc" : "desc");
+        }
+    }
+
+    function compareOrdersByName(a: Order, b: Order) {
+        const yearComparison = a.year - b.year;
+        return yearComparison === 0 ? a.id - b.id : yearComparison;
+    }
+
+    function compareOrdersByDeliveryDate(a: Order, b: Order) {
+        if (a.scheduledDeliveryDate && b.scheduledDeliveryDate) {
+            return a.scheduledDeliveryDate.localeCompare(b.scheduledDeliveryDate);
+        } else if (a.scheduledDeliveryDate) {
+            return -1;
+        } else if (b.scheduledDeliveryDate) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    function compareOrders(a: Order, b: Order) {
+        switch (props.comparison) {
+            case "name": {
+                const nameComparison = compareOrdersByName(a, b);
+                return props.comparisonOrder === "asc" ? nameComparison : -1 * nameComparison;
+            }
+            case "job": {
+                const jobComparison = a.job.id.localeCompare(b.job.id);
+                return props.comparisonOrder === "asc" ? jobComparison : -1 * jobComparison;
+            }
+            case "supplier": {
+                const supplierComparison = a.supplier.localeCompare(b.supplier);
+                return props.comparisonOrder === "asc" ? supplierComparison : -1 * supplierComparison;
+            }
+            case "deliveryDate": {
+                const deliveryDateComparison = compareOrdersByDeliveryDate(a, b);
+                return props.comparisonOrder === "asc" ? deliveryDateComparison : -1 * deliveryDateComparison;
+            }
+        }
+    }
+
+    return (
+        <Row className="glossy-background w-100">
+            <Row className="mb-2">
+                <Col className="d-flex justify-content-between">
+                    <ArrowLeftSquare
+                        className={decreasablePageNumber ? "clickable-arrow" : "unclickable-arrow"}
+                        onClick={decreasePageNumber}/>
+                    <p className="text-center">
+                        Pagina {pageNumber + 1} di {Math.ceil(props.orders.length / 100)}
+                    </p>
+                    <ArrowRightSquare
+                        className={increasablePageNumber ? "clickable-arrow" : "unclickable-arrow"}
+                        onClick={increasePageNumber}/>
+                </Col>
+            </Row>
+
+            <Table hover responsive className="orders-table">
+                <thead>
+                <tr>
+                    <th className="comparable"
+                        onClick={() => selectComparison("name")}>Ordine {showCaret("name")}</th>
+                    <th className="comparable"
+                        onClick={() => selectComparison("job")}>Commessa {showCaret("job")}</th>
+                    {!props.shrunkTable && <th>Data</th>}
+                    <th className="comparable"
+                        onClick={() => selectComparison("supplier")}>Fornitore {showCaret("supplier")}</th>
+                    {!props.shrunkTable && <>
+                        <th>Descrizione</th>
+                        <th>Presa in carico da</th>
+                        <th className="comparable"
+                            onClick={() => selectComparison("deliveryDate")}>Consegna {showCaret("deliveryDate")}</th>
+                        <th>Evasa da</th>
+                        <th>Evasa il</th>
+                        <th>Allegato</th>
+                    </>}
+                </tr>
+                </thead>
+
+                <tbody>
+                {props.orders
+                    .filter(filterOrders)
+                    .sort(compareOrders)
+                    .slice(pageNumber * 100, (pageNumber + 1) * 100)
+                    .map(order => {
+                        let className = "";
+                        if (order.id === props.selectedOrder?.id) {
+                            className = "table-selected-row";
+                        }
+                        if (order.clearedBy && order.clearingDate) {
+                            className += " cleared";
+                        }
+
+                        return (
+                            <tr key={order.name} onClick={() => props.selectOrder(order)}
+                                className={className}>
+                                <td>{order.name}</td>
+                                <td>{order.job.id}</td>
+                                {!props.shrunkTable && <td>{formatDate(order.date)}</td>}
+                                <td>{order.supplier}</td>
+                                {!props.shrunkTable && <>
+                                    <td>{order.description}</td>
+                                    <td>{order.by.surname} {order.by.name}</td>
+                                    <td>{formatDate(order.scheduledDeliveryDate)}</td>
+                                    <td>{order.clearedBy?.surname} {order.clearedBy?.name}</td>
+                                    <td>{formatDate(order.clearingDate)}</td>
+                                    {order.uploadedFile ? <td>
+                                        <TextButton icon={FileEarmark} onClick={() => {
+                                            const attachmentOrderName = `${order.id}-${order.year.toString().substring(2)}`;
+                                            navigate(`/order/${attachmentOrderName}/pdf`);
+                                        }}>Apri</TextButton>
+                                    </td> : <td/>}
+                                </>}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+        </Row>
+    );
+}
+
+export default OrdersTable;
