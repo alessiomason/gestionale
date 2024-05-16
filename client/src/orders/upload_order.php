79df3e435dd33ff04da4f838aa5068484000dc2a
Alessio Mason <alessio.mason@me.com>
2024-05-16 15:03:00 +0200

    Access control and checks in PHP files

--- client/src/orders/upload_order.php
+++ client/src/orders/upload_order.php
@@ -2,19 +2,33 @@
 // This file is not part of the React client, but is meant to be uploaded on an Apache server where the uploaded order
 // files are stored.
 
-header("Access-Control-Allow-Origin: https://gestionale.tlftechnology.it");
+$allowed_host = "https://gestionale.tlftechnology.it";
+
+header("Access-Control-Allow-Origin: $allowed_host");
+
+if (!isset($_SERVER["HTTP_REFERER"]) || parse_url($_SERVER["HTTP_REFERER"])["host"] !== parse_url($allowed_host)["host"]) {
+    http_response_code(401);
+    return;
+}
 
 if (!isset($_FILES["orderFile"])) {
     http_response_code(422);
     return;
 }
 
-$uploadDir = "orders/";
-$fileName = basename($_FILES["orderFile"]["name"]);
+$upload_dir = "orders/";
+$file_name = basename($_FILES["orderFile"]["name"]);
+$file_extension = pathinfo($_FILES["orderFile"]["name"], PATHINFO_EXTENSION);
 
-if (!move_uploaded_file($_FILES["orderFile"]["tmp_name"], $uploadDir . $fileName)) {
+if (strtolower($file_extension) !== "pdf") {
+    http_response_code(422);
+    return;
+}
+
+if (!move_uploaded_file($_FILES["orderFile"]["tmp_name"], $upload_dir . $file_name)) {
     http_response_code(422);
     return;
 }
 
 http_response_code(200);
+exit;
--- server/src/database/backup.php
+++ server/src/database/backup.php
@@ -1,17 +1,33 @@
 <?php
 // This file is not part of the Express server, but is meant to be uploaded on an Apache server where the backups are stored.
 
+$allowed_host = "https://gestionale.tlftechnology.it";
+
+header("Access-Control-Allow-Origin: $allowed_host");
+
+if (!isset($_SERVER["HTTP_REFERER"]) || parse_url($_SERVER["HTTP_REFERER"])["host"] !== parse_url($allowed_host)["host"]) {
+    http_response_code(401);
+    return;
+}
+
 if (!isset($_FILES["backupFile"])) {
     http_response_code(422);
     return;
 }
 
-$uploadDir = "backups/";
-$fileName = basename($_FILES["backupFile"]["name"]);
+$upload_dir = "backups/";
+$file_name = basename($_FILES["backupFile"]["name"]);
+$file_extension = pathinfo($_FILES["backupFile"]["name"], PATHINFO_EXTENSION);
 
-if (!move_uploaded_file($_FILES["backupFile"]["tmp_name"], $uploadDir . $fileName)) {
+if (strtolower($file_extension) !== "sql") {
+    http_response_code(422);
+    return;
+}
+
+if (!move_uploaded_file($_FILES["backupFile"]["tmp_name"], $upload_dir . $file_name)) {
     http_response_code(422);
     return;
 }
 
 http_response_code(200);
+exit;
