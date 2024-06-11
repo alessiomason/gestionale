7a4ce8586221cbc1b6a0da7dba554521565afecd
Alessio Mason <alessio.mason@me.com>
2024-06-11 09:09:57 +0200

    Changed PDF viewer from object tag to iframe

--- client/src/orders/order-pdf-viewer/OrderPDFViewer.tsx
+++ client/src/orders/order-pdf-viewer/OrderPDFViewer.tsx
@@ -7,9 +7,9 @@ function OrderPDFViewer() {
     const attachmentLocation = `${process.env.REACT_APP_ORDERS_PDF_FOLDER}/get_order.php?orderName=${orderName}`;
 
     return (
-        <object title={orderName} data={attachmentLocation} type="application/pdf" className="pdf-viewer">
+        <iframe title={orderName} src={attachmentLocation} className="pdf-viewer">
             <h1>Impossibile visualizzare il file dell'ordine {orderName}.</h1>
-        </object>
+        </iframe>
     );
 }
 
