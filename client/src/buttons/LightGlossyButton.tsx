063b92e5322377a21d73590b6fad158ae7f58c28
Alessio Mason <alessio.mason@me.com>
2024-06-17 10:32:26 +0200

    Secondary icon in buttons

--- client/src/buttons/LightGlossyButton.tsx
+++ client/src/buttons/LightGlossyButton.tsx
@@ -5,21 +5,25 @@ import "./Buttons.css";
 
 interface LightGlossyButtonProps {
     readonly children?: string
+    readonly type?: "submit" | "reset" | "button"
     readonly icon: Icon
+    readonly secondaryIcon?: Icon
     readonly singleLine?: boolean
     readonly className?: string
     readonly onClick: (() => void) | ((event: MouseEvent<HTMLButtonElement>) => void)
 }
 
 function LightGlossyButton(props: LightGlossyButtonProps) {
-    const margin = props.children ? "me-1" : "";
+    const iconMargin = props.children ? "me-1" : "";
+    const secondaryIconMargin = props.children ? "ms-1" : "";
 
     return (
-        <Button
+        <Button type={props.type}
             className={`light-glossy-button d-flex justify-content-center align-items-center ${props.className} ${props.singleLine ? "single-line-button" : undefined}`}
             onClick={props.onClick}>
-            {createElement(props.icon, {className: margin})}
+            {createElement(props.icon, {className: iconMargin})}
             {props.children}
+            {props.secondaryIcon && createElement(props.secondaryIcon, {className: secondaryIconMargin})}
         </Button>
     );
 }
