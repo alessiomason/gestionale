536ac54bedbee01f40c06eecc7c024081551e62c
Alessio Mason <alessio.mason@me.com>
2024-09-07 11:25:55 +0200

    Color planning table cells based on user type

--- client/src/planning/PlanningTable.tsx
+++ client/src/planning/PlanningTable.tsx
@@ -217,7 +217,7 @@ function PlanningTable(props: PlanningTableProps) {
 
                                                     return (
                                                         <td key={`${user.id}-${workday.format()}`}
-                                                            className={workdayClassName(workday, true)}
+                                                            className={`${workdayClassName(workday, true)} ${type}-user`}
                                                             onClick={() => handleDayClick(user, workday, plannedDay?.job)}>
                                                             {plannedDay?.job.client.substring(0, 2).toUpperCase() ?? ""}
                                                         </td>
