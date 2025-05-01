b01150caa24dc2b0b7b5fca14800c813d6a890c5
Alessio Mason <alessio.mason@me.com>
2025-05-01 17:13:50 +0200

    Updated email service for TM deployment
    Using Gmail

--- server/src/email/emailService.ts
+++ server/src/email/emailService.ts
@@ -1,16 +1,35 @@
 import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
+import {OAuth2Client} from "google-auth-library";
 
+// Nodemailer guide at https://nodemailer.com/smtp/oauth2/
+// followed guide at https://medium.com/@nickroach_50526/sending-emails-with-node-js-using-smtp-gmail-and-oauth2-316fe9c790a1#
+// additional step at https://github.com/nodemailer/nodemailer/issues/266#issuecomment-542791806
 export async function sendEmail(to: string, subject: string, html: string, text: string) {
+    const oauth2Client = new OAuth2Client(
+        process.env.EMAIL_CLIENT_ID,
+        process.env.EMAIL_CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+    );
+    oauth2Client.setCredentials({
+        refresh_token: process.env.EMAIL_REFRESH_TOKEN
+    });
+    const accessToken = (await oauth2Client.getAccessToken()).token;
+
     const smtpTransport = nodemailer.createTransport({
-        service: "Outlook365",
+        host: "smtp.gmail.com",
+        port: 465,
         secure: true,
         auth: {
+            type: "OAuth2",
             user: process.env.EMAIL,
-            pass: process.env.EMAIL_PASSWORD
+            clientId: process.env.EMAIL_CLIENT_ID,
+            clientSecret: process.env.EMAIL_CLIENT_SECRET,
+            refreshToken: process.env.EMAIL_REFRESH_TOKEN,
+            accessToken: accessToken ?? undefined   // if null, set undefined
         },
         tls: {
-            ciphers: "SSLv3"
+            rejectUnauthorized: false
         }
     });
 
